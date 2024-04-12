d18ad9be7b83a9ae54d9affc1c6d7b5599b4674b
liuj012 <145478562+liuj012@users.noreply.github.com>
2024-04-12 11:14:16 -0400

    change layout to widespread

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-      $(".works").draggable({
-        containment: "window"
-      });
-    });
+  var maxLeft = ($(window).width() * 0.98) - $('.works').outerWidth();
+  var maxTop = ($(window).height() * 0.98) - $('.works').outerHeight();
+      
+  $(".works").draggable({
+  containment: [0, 0, maxLeft, maxTop]
+  });
+});
 
 
