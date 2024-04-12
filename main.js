ed7743cc4050f20bb3c1cc816a5a53d6ceae12e4
liuj012 <145478562+liuj012@users.noreply.github.com>
2024-04-12 11:31:18 -0400

    more positioning

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  var maxLeft = ($(window).width() * 0.98) - $('.works').outerWidth();
-  var maxTop = ($(window).height() * 0.98) - $('.works').outerHeight();
+  var maxLeft = ($(window).width() * 0.96) - $('.works').outerWidth();
+  var maxTop = ($(window).height() * 0.96) - $('.works').outerHeight();
       
   $(".works").draggable({
   containment: [0, 0, maxLeft, maxTop]
