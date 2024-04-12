ee1f80088860365375fd03357de8b379afe77fb5
liuj012 <145478562+liuj012@users.noreply.github.com>
2024-04-11 22:17:40 -0400

    new look

--- main.js
+++ main.js
@@ -1,22 +1,5 @@
 $(document).ready(function() {
-
-  $(window).scroll(scrollEffect);
-
-  function scrollEffect() {
-    if ($(window).scrollTop() >= 300) {
-      $('.works').css({
-        'opacity': '1',
-        'transform': 'translateY(0)',
-        'transition': '1s ease-in-out'
-      });
-    } else {
-      $('.works').css({
-        'opacity': '0',
-        'transform': 'translateY(50px)',
-        'transition': '1s ease-in-out'
-      });
-    }
-  }
-});
+      $(".works").draggable();
+    });
 
 
