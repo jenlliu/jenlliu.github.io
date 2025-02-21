1121f8c18ccb664d79ff7b3b71b761b841b52078
liuj012 <145478562+liuj012@users.noreply.github.com>
2025-02-21 12:23:44 -0500

    fixing things

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 
 $(document).ready(function () {
     let images = [
-        { src: "imgs/housenumbers.png", title: "Where Numbers Rest, 2024", fraction: "1/4" },
-        { src: "imgs/belt.gif", title: "On Belt Buckles, 2024", fraction: "2/4" },
-        { src: "imgs/knitfullscan.png", title: "Born to be Somebody, 2024", fraction: "3/4" },
-        { src: "imgs/home.png", title: "Found Homes, 2024", fraction: "4/4" }
+        { src: "imgs/housenumbers.png", title: "<a href=\"https://jenlliu.com/wherenumbers/\">Where Numbers Rest, 2024</a>", fraction: "1/4" },
+        { src: "imgs/belt.gif", title: "<a href=\"https://jenlliu.com/onbeltbuckles/\">On Belt Buckles, 2024</a>", fraction: "2/4" },
+        { src: "imgs/knitfullscan.png", title: "<a href=\"https://jenlliu.com/borntobe/\">Born to be Somebody, 2024</a>", fraction: "3/4" },
+        { src: "imgs/home.png", title: "<a href=\"https://jenlliu.com/foundhomes/\">Found Homes, 2024</a>", fraction: "4/4" }
     ];
 
     let index = 0;
 
-    $(".selectedworks").html(`<img src="${images[0].src}" class="active" alt="${images[0].title}">`);
+    $(".selectedworks").html(`<img src="${images[0].src}" class="active">`);
     $(".caption").html(`
         <span class="title">${images[0].title}</span>
         <span class="fraction">${images[0].fraction}</span>
