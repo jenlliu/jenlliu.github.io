fb794b97c33027a42ef2dc447c9fa7faedd77c70
liuj012 <145478562+liuj012@users.noreply.github.com>
2025-04-02 22:28:58 -0400

    new home page

--- main.js
+++ main.js
@@ -1,30 +1,71 @@
+$(document).ready(function(){
+    $('.index').click(function (){
+        $('.indexpopup').removeClass('hidden');
+    })
 
-$(document).ready(function () {
-    let images = [
-        { src: "imgs/housenumbers.png", title: "<a href=\"https://jenlliu.com/wherenumbers/\">Where Numbers Rest, 2024</a>", fraction: "1/4" },
-        { src: "imgs/belt.gif", title: "<a href=\"https://jenlliu.com/onbeltbuckles/\">On Belt Buckles, 2024</a>", fraction: "2/4" },
-        { src: "imgs/knitfullscan.png", title: "<a href=\"https://jenlliu.com/borntobe/\">Born to be Somebody, 2024</a>", fraction: "3/4" },
-        { src: "imgs/home.png", title: "<a href=\"https://jenlliu.com/foundhomes/\">Found Homes, 2024</a>", fraction: "4/4" }
-    ];
+    $('.close').click(function (){
+        $('.indexpopup').addClass('hidden');
+    })
+})
 
-    let index = 0;
 
-    $(".selectedworks").html(`<img src="${images[0].src}" class="active">`);
+let images = [
+    { 
+        src: "<video width='100%' height='auto' autoplay loop muted><source src='imgs/borntobe.mp4' type='video/mp4'>Your browser does not support the video tag.</video>", 
+        title: "<a href=\"https://jenlliu.com/borntobe/\">Born to be Somebody, 2024</a>",
+        textcolor: "black"
+    },
+    { 
+        src: "imgs/numbers.webp", 
+        title: "<a href=\"https://jenlliu.com/wherenumbers/\">Where Numbers Rest, 2024</a>",
+        textcolor: "white"
+    },
+    { 
+        src: "imgs/lb.webp",
+        title: "<a href=\"https://jenlliu.com/loosebuttons/\">Loose Buttons, 2025</a>", 
+        textcolor: "white"
+    },
+    { 
+        src: "imgs/home.webp",
+        title: "<a href=\"https://jenlliu.com/foundhomes/\">Found Homes in New York, 2024</a>", 
+        textcolor: "black"
+    },
+    { 
+        src: "imgs/belt.webp",
+        title: "<a href=\"https://jenlliu.com/onbeltbuckles/\">On Belt Buckles, 2024</a>", 
+        textcolor: "black"
+    },
+    { 
+        src: "imgs/azxlb.webp",
+        title: "<a href=\"https://jenlliu.com/azxlb/\">Amanda Zheng by LB, 2025</a>", 
+        textcolor: "white"
+    }
+];
+
+let index = 0;
+
+function updateContent() {
+    let currentItem = images[index];
+    if (currentItem.src.includes("<video")) {
+        $(".selectedworks").html(currentItem.src);
+    } else {
+        $(".selectedworks").html(`<img src="${currentItem.src}" class="active" alt="Image">`);
+    }
+
     $(".caption").html(`
-        <span class="title">${images[0].title}</span>
-        <span class="fraction">${images[0].fraction}</span>
+        <span class="title">${currentItem.title}</span>
     `);
 
-    $(".selectedworks").click(function () {
-        index = (index + 1) % images.length;
+    if (currentItem.textcolor.includes("white")) {
+        $(".homepagelink, .caption a, .info").addClass('white');
+    } else {
+        $(".homepagelink, .caption a, .info").removeClass('white');
+    }
+}
 
-        $(".selectedworks img").attr("src", images[index].src).attr("alt", images[index].title);
+updateContent();
 
-        $(".caption").html(`
-            <span class="title">${images[index].title}</span>
-            <span class="fraction">${images[index].fraction}</span>
-        `);
-    });
+$(".selectedworks").click(function () {
+    index = (index + 1) % images.length;
+    updateContent(); 
 });
-
-
